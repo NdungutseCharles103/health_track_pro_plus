6b25fea8d70d8cdc9585fbeb5ca4cf92fe9ad050
Ndungutse Charles <95475354+NdungutseCharles103@users.noreply.github.com>
2023-11-02 19:19:59 +0200

    feat: update backend url

--- client/src/utils/fetch.ts
+++ client/src/utils/fetch.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.PROD ? 'https://health-track-pro-plus.onrender.com/api' : 'http://localhost:3010/api';
+
 export const api = axios.create({
-   baseURL: import.meta.env.VITE_API_URL,
+   baseURL: apiUrl,
 });
